2f1d09dcdec195fe8edce0c2451b10ae7eb4c5bc
AidenCastillo <amc-mcjarjar@hotmail.com>
2024-05-06 00:10:16 -0500

    Fixed VertexBufferLayout combatabillity for gcc

--- EonEngine/src/core/Renderer.h
+++ EonEngine/src/core/Renderer.h
@@ -6,10 +6,6 @@
 #include "demoShader.h"
 #include <iostream>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__));
 
 void GLClearError();
 
--- EonEngine/src/core/VertexBufferLayout.h
+++ EonEngine/src/core/VertexBufferLayout.h
@@ -35,27 +35,25 @@ public:
 	{
 	}
 
-	template<>
-	void Push<float>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
-	}
 
-	template<>
-	void Push<unsigned int>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
-	}
 
-	template<>
-	void Push<unsigned char>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
-	}
 
 	inline const std::vector<VertexBufferElement> GetElements() const& { return m_Elements; }
 	inline unsigned int GetStride() const { return m_Stride; }
 };
+
+template<> inline void VertexBufferLayout::Push<float>(unsigned int count)
+{
+    m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
+    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
+}
+template<> inline void VertexBufferLayout::Push<unsigned int>(unsigned int count)
+{
+    m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
+    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
+}
+template<> inline void VertexBufferLayout::Push<unsigned char>(unsigned int count)
+{
+    m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
+    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
+}
